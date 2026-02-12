2927a717321d8ff93d4fc9de042adbe315129bbe
YSchilling <78358953+YSchilling@users.noreply.github.com>
2026-02-12 17:41:58 +0100

    part names as variables

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+# install.sh
 
 set -euo pipefail # activate secure running of script
 
 SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-echo "Starting subscripts\n"
+echo "Starting subscripts"
 
 source "$SCRIPT_DIR/config.sh"
 source "$SCRIPT_DIR/scripts/partition.sh"
--- scripts/partition.sh
+++ scripts/partition.sh
@@ -1,12 +1,19 @@
-#!/bin/bash
+# scripts/partition.sh
 
 if [[ -z "${diskname:-}" ]]; then
     echo "Error: variable \$diskname is not set"
     exit 1
 fi
 
+# adds "p" if the diskname ends with a number (nvme0n1, ...)
+suffix=""
+[[ "$diskname" =~ [0-9]$ ]] && suffix="p"
+
+part1="${diskname}${suffix}1"
+part2="${diskname}${suffix}2"
+
 # create partitions
-echo "Partition: create partitions\n"
+echo "Partition: create partitions"
 parted -s "$diskname" mklabel gpt
 
 parted -s "$diskname" mkpart "EFI" fat32 1MiB 1025MiB
@@ -16,13 +23,13 @@ parted -s "$diskname" mkpart "root" ext4 1025MiB 100%
 parted -s "$diskname" type 2 4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709
 
 # create filesystems
-echo "Partition: create filesystems\n"
-mkfs.fat -F32 "${diskname}1"
-mkfs.ext4 "${diskname}2"
+echo "Partition: create filesystems"
+mkfs.fat -F32 "$part1"
+mkfs.ext4 -F "$part2"
 
 # mount filesystems
-echo "Partition: mount filesystems\n"
-mkdir /mnt/boot
+echo "Partition: mount filesystems"
+mkdir /mnt/boot -p
 
-mount "${diskname}2 /mnt
-mount "${diskname}1 /mnt/boot
+mount "$part2" /mnt
+mount "$part1" /mnt/boot
