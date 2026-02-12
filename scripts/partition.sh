a99c3e398046bf2515e9255605eaf7f25dc87991
YSchilling <78358953+YSchilling@users.noreply.github.com>
2026-02-12 17:14:34 +0100

    adjusted partition names

--- scripts/partition.sh
+++ scripts/partition.sh
@@ -7,8 +7,8 @@ fi
 
 parted -s "$diskname" mklabel gpt
 
-parted -s "$diskname" mkpart "EFI system partition" fat32 1MiB 1025MiB
+parted -s "$diskname" mkpart "EFI" fat32 1MiB 1025MiB
 parted -s "$diskname" set 1 esp on
 
-parted -s "$diskname" mkpart "root partition" ext4 1025MiB 100%
+parted -s "$diskname" mkpart "root" ext4 1025MiB 100%
 parted -s "$diskname" type 2 4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709
