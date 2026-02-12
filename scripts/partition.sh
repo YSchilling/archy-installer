5655dac0edee2b0ce7d77ae18c8e9891f77e4ec1
YSchilling <78358953+YSchilling@users.noreply.github.com>
2026-02-12 18:19:16 +0100

    fix swap syntax

--- scripts/partition.sh
+++ scripts/partition.sh
@@ -20,10 +20,10 @@ parted -s "$diskname" mklabel gpt
 parted -s "$diskname" mkpart "EFI" fat32 1MiB 1025MiB
 parted -s "$diskname" set 1 esp on
 
-parted -s "$diskname" mkpart "root" ext4 1025MiB "100%-${swapsize}"
+parted -s "$diskname" mkpart "root" ext4 1025MiB "-${swapsize}"
 parted -s "$diskname" type 2 4F68BCE3-E8CD-4DB1-96E7-FBCAF984B709
 
-parted -s "$diskname" mkpart "swap" linux-swap "100%-${swapsize}" 100%
+parted -s "$diskname" mkpart "swap" linux-swap "-${swapsize}" 100%
 parted -s "$diskname" type 3 0657FD6D-A4AB-43C4-84E5-0933C84B4F4F
 
 # wait for kernel to catch on part creations
